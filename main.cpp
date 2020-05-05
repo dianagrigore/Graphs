32834d052f6458192df83cf68ec5194c8420df47
dianagrigore <diana.grigore12@yahoo.com>
2020-05-05 11:22:01 +0300

    Update main

--- main.cpp
+++ main.cpp
@@ -1,43 +1,27 @@
 #include <iostream>
+#include <fstream>
 #include "Vector.h"
 #include "MatriceAdiacenta.h"
 #include "Lista.h"
 #include "Graf.h"
 #include "Graf_Orientat.h"
 #include "Graf_Neorientat.h"
-#include <fstream>
+
+
+
+void testeaza_graf(Graf& graf, std :: istream& f){
+    int nod_start;
+    f >> nod_start;
+    f >> graf;
+    graf.afisare_rezultat(nod_start);
+}
 
 int main() {
     std :: ifstream f("graf.in");
-    int nr_neorientate, nod_start, nr_orientate, m;
-    f >> nr_orientate >> nr_neorientate;
-    Graf_Orientat* grafuriorientate;
-    grafuriorientate = new Graf_Orientat[nr_orientate];
-    for (int i = 0; i < nr_orientate; i++){
-        f >> nod_start;
-        f >> grafuriorientate[i];
-        try {
-                m = grafuriorientate[i].isgraf();
-                throw m;
-        }
-        catch(int m)
-        {   if (m == 0)
-                std :: cout << "Matrice de adiacenta invalida ";
-            else
-                grafuriorientate[i].f(nod_start);
-        }
-
-    }
-    Graf_Neorientat* grafurineorientate;
-    grafurineorientate = new Graf_Neorientat[nr_neorientate];
-    for (int i = 0; i < nr_neorientate; i++){
-        f >> nod_start;
-        f >> grafurineorientate[i];
-        grafurineorientate[i].f(nod_start);
-    }
-    delete [] grafurineorientate;
-    delete  [] grafuriorientate;
+    Graf_Orientat go;
+    Graf_Neorientat gn;
+    testeaza_graf(go, f);
+    testeaza_graf(gn, f);
     f.close();
-
     return 0;
 }
